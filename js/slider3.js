083258ec6efc999251500958659f7aa772e24d65
san64n4 <mueblesmetalicosayg@gmail.com>
2024-04-29 12:44:59 -0500

    subir web
    subir pagina web

---
+++ js/slider3.js
@@ -0,0 +1,31 @@
+(function(){
+    
+    const sliders = [...document.querySelectorAll('.testimony__body3')];
+    const buttonNext = document.querySelector('#next3');
+    const buttonBefore = document.querySelector('#before3');
+    let value;   
+
+    buttonNext.addEventListener('click', ()=>{
+        changePosition(1);
+    });
+
+    buttonBefore.addEventListener('click', ()=>{
+        changePosition(-1);
+    });
+
+    const changePosition = (add)=>{
+        const currentTestimony = document.querySelector('.testimony__body--show3').dataset.id;
+        value = Number(currentTestimony);
+        value+= add;
+
+
+        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show3');
+        if(value === sliders.length+1 || value === 0){
+            value = value === 0 ? sliders.length  : 1;
+        }
+
+        sliders[value-1].classList.add('testimony__body--show3');
+
+    }
+
+})();
